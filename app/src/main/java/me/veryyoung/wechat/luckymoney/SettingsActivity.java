78881fa7be70e9778f19f167147dbbd430c722c9
fwt55 <fwt555@gmail.com>
2018-03-20 00:15:30 +0800

    demo完成

--- app/src/main/java/me/veryyoung/wechat/luckymoney/SettingsActivity.java
+++ app/src/main/java/me/veryyoung/wechat/luckymoney/SettingsActivity.java
@@ -2,15 +2,23 @@ package me.veryyoung.wechat.luckymoney;
 
 
 import android.annotation.TargetApi;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.app.FragmentManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceFragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -26,13 +34,53 @@ public class SettingsActivity extends AppCompatActivity {
             replaceFragment(R.id.settings_container, mSettingsFragment);
         }
 
+//        IntentFilter filter = new IntentFilter();
+//        filter.addAction("me.veryyoung.wechat.luckymoney.wechat_msg_received");
+//        registerReceiver(msgReceiver, filter);
     }
-
 
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     public void replaceFragment(int viewId, android.app.Fragment fragment) {
         FragmentManager fragmentManager = getFragmentManager();
         fragmentManager.beginTransaction().replace(viewId, fragment).commit();
+    }
+
+    private BroadcastReceiver msgReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (action != null && action.equals("me.veryyoung.wechat.luckymoney.wechat_msg_received")) {
+                int msgType = intent.getIntExtra("msgType", 0);
+                String content = intent.getStringExtra("content");
+
+//                showNormalDialog(msgType, content);
+                Toast.makeText(getApplicationContext(), content, Toast.LENGTH_LONG).show();
+            }
+        }
+    };
+
+
+    private void showNormalDialog(int msgType, String content) {
+        final AlertDialog.Builder normalDialog =
+                new AlertDialog.Builder(this);
+        normalDialog.setTitle("消息类型："+msgType);
+        normalDialog.setMessage("content");
+        normalDialog.setPositiveButton("确定",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //...To-do
+                    }
+                });
+        normalDialog.setNegativeButton("关闭",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //...To-do
+                    }
+                });
+        // 显示
+        normalDialog.show();
     }
 
 
